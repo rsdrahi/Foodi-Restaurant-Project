b63323ed8d28c613a2d15e8fc1226d90649f1f10
rsdrahi <rsdrahi13@gmail.com>
2026-04-01 23:54:40 +0600

    navbar compleated as per fig file

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
+import { Handbag, PhoneCall, Search } from 'lucide-react';
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className='flex  justify-between items-center mt-2 shadow p-2 '> 
+      <h2 className='font-bold text-3xl'>
+        <span className='font-bold text-3xl bg-green-500 px-3 rounded-lg text-white'>f</span>
+        OODI
+      </h2>
       
-    </div>
+      <ul className='flex gap-18 font-medium'>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Services</li>
+        <li>Offers</li>
+      </ul>
+      <div className='flex gap-12 justify-center items-center'>
+         <Search></Search>
+      <Handbag></Handbag>
+
+          <button className='flex gap-4 btn bg-[#39DB4A] text-white rounded-full m-2 justify-center items-center p-6' ><PhoneCall></PhoneCall>Contact</button>
+
+     </div>
+    </nav>
   );
 };
 
